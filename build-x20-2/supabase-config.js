37621cc4b104c7f2c4713634ea6a365740611681
Rajkannaujiya06 <rajkannaujiya820@gmail.com>
2026-09-11 22:20:11 +0530

    Add files via upload

---
+++ build-x20-2/supabase-config.js
@@ -0,0 +1,6 @@
+/* Public browser configuration. The publishable key is designed to be exposed
+   in a frontend; database and storage policies in schema.sql enforce access. */
+window.AKTU_SUPABASE_CONFIG = {
+  url: 'https://rseqtsdhmbsyarpltrke.supabase.co',
+  publishableKey: 'sb_publishable_l4MTjGMqA0T0tjElDYuQ0Q_YwbTz8ej'
+};
